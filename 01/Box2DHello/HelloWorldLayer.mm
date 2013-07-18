05b869aeb04e76977860a69b3e30b05a823a58b2
curer <studentdeng@hotmail.com>
2013-07-18 17:15:10 +0800

    create 01

---
+++ 01/Box2DHello/HelloWorldLayer.mm
@@ -0,0 +1,174 @@
+//
+//  HelloWorldLayer.mm
+//  Box2DHello
+//
+//  Created by curer on 7/18/13.
+//  Copyright curer 2013. All rights reserved.
+//
+
+// Import the interfaces
+#import "HelloWorldLayer.h"
+
+// Not included in "cocos2d.h"
+#import "CCPhysicsSprite.h"
+
+// Needed to obtain the Navigation Controller
+#import "AppDelegate.h"
+
+
+enum {
+	kTagParentNode = 1,
+};
+
+
+#pragma mark - HelloWorldLayer
+
+@interface HelloWorldLayer()
+-(void) initPhysics;
+-(void) addNewSpriteAtPosition:(CGPoint)p;
+-(void) createMenu;
+@end
+
+@implementation HelloWorldLayer
+
++(CCScene *) scene
+{
+	// 'scene' is an autorelease object.
+	CCScene *scene = [CCScene node];
+	
+	// 'layer' is an autorelease object.
+	HelloWorldLayer *layer = [HelloWorldLayer node];
+	
+	// add layer as a child to scene
+	[scene addChild: layer];
+	
+	// return the scene
+	return scene;
+}
+
+-(id) init
+{
+	if( (self=[super init])) {
+		
+		// enable events
+		
+		self.touchEnabled = YES;
+		CGSize s = [CCDirector sharedDirector].winSize;
+		
+		// init physics
+		[self initPhysics];
+		
+		[self addSpriteAtPosition:ccp(s.width/2, s.height/2)];
+		
+		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Tap screen" fontName:@"Marker Felt" fontSize:32];
+		[self addChild:label z:0];
+		[label setColor:ccc3(0,0,255)];
+		label.position = ccp( s.width/2, s.height-50);
+		
+		[self scheduleUpdate];
+	}
+	return self;
+}
+
+-(void) dealloc
+{
+	delete world;
+	world = NULL;
+	
+	[super dealloc];
+}
+
+-(void) initPhysics
+{
+	CGSize s = [[CCDirector sharedDirector] winSize];
+	
+	b2Vec2 gravity;
+	gravity.Set(0.0f, -10.0f);
+	world = new b2World(gravity);
+	
+	// Define the ground body.
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0, 0); // bottom-left corner
+	
+	// Call the body factory which allocates memory for the ground body
+	// from a pool and creates the ground box shape (also from a pool).
+	// The body is also added to the world.
+	b2Body* groundBody = world->CreateBody(&groundBodyDef);
+	
+	// Define the ground box shape.
+	b2EdgeShape groundBox;		
+	
+	// bottom
+	
+	groundBox.Set(b2Vec2(0,0), b2Vec2(s.width/PTM_RATIO,0));
+	groundBody->CreateFixture(&groundBox,0);
+	
+	// top
+	groundBox.Set(b2Vec2(0,s.height/PTM_RATIO), b2Vec2(s.width/PTM_RATIO,s.height/PTM_RATIO));
+	groundBody->CreateFixture(&groundBox,0);
+	
+	// left
+	groundBox.Set(b2Vec2(0,s.height/PTM_RATIO), b2Vec2(0,0));
+	groundBody->CreateFixture(&groundBox,0);
+	
+	// right
+	groundBox.Set(b2Vec2(s.width/PTM_RATIO,s.height/PTM_RATIO), b2Vec2(s.width/PTM_RATIO,0));
+	groundBody->CreateFixture(&groundBox,0);
+}
+
+- (void)addSpriteAtPosition:(CGPoint)pt
+{
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(pt.x/PTM_RATIO, pt.y/PTM_RATIO);
+    body = world->CreateBody(&bodyDef);
+    
+    b2CircleShape bodyShape;
+    bodyShape.m_radius = 26.0 / PTM_RATIO;
+    
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &bodyShape;
+    fixtureDef.density = 0.1f;
+    fixtureDef.friction = 0.1f;
+    fixtureDef.restitution = 0.9f;
+    body->CreateFixture(&fixtureDef);
+    
+    CCPhysicsSprite *sprite = [CCPhysicsSprite spriteWithFile:@"ball.png"];
+    [self addChild:sprite];
+    
+    [sprite setPTMRatio:PTM_RATIO];
+    [sprite setB2Body:body];
+    [sprite setPosition: ccp(pt.x, pt.y)];
+}
+
+-(void) update: (ccTime) dt
+{
+	//It is recommended that a fixed time step is used with Box2D for stability
+	//of the simulation, however, we are using a variable time step here.
+	//You need to make an informed choice, the following URL is useful
+	//http://gafferongames.com/game-physics/fix-your-timestep/
+	
+	int32 velocityIterations = 8;
+	int32 positionIterations = 1;
+	
+	// Instruct the world to perform a single step of simulation. It is
+	// generally best to keep the time step and iterations fixed.
+	world->Step(dt, velocityIterations, positionIterations);	
+}
+
+- (void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	//Add a new body/atlas sprite at the touched location
+	for( UITouch *touch in touches ) {
+		CGPoint location = [touch locationInView: [touch view]];
+		
+		location = [[CCDirector sharedDirector] convertToGL: location];
+		
+		[self addSpriteAtPosition:location];
+	}
+    
+    b2Vec2 force = b2Vec2(-30, 30);
+    body->ApplyLinearImpulse(force, body->GetPosition());
+}
+
+@end
